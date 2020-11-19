585e81c1209bba5db250f7799460e11c89b70cd6
VitorFranco <vitoroliveirafranco@gmail.com>
2020-11-19 18:17:35 -0300

    Refatoração do código.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,6 @@
 const formatDate = (date: Date): string => {
   const dateExpression = new Date(date);
-  const handleDateMonth = dateExpression.getMonth() + 1;
-  const handleDateDay = dateExpression.getDate();
-  const handleDateYear = dateExpression.getFullYear();
 
-  return `${handleDateDay}/${handleDateMonth}/${handleDateYear}`;
+  return dateExpression.toLocaleDateString('pt-BR');
 };
 export default formatDate;
